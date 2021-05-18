f9ad1f47ee40ef834032bdb4963aefd5be49cf40
Vjeko Konc <vjeko.konc@gmail.com>
2021-05-18 05:37:42 +0200

    Change following link into following/live

--- src/js/custom-visual-fixes.js
+++ src/js/custom-visual-fixes.js
@@ -3,4 +3,10 @@ console.log("Applying own fixes to the user interface.");
 setInterval(function () {
   // Remove the ugly default hover effect
   document.querySelectorAll(".ScWrapper-uo2e2v-0").forEach((x) => x.classList.remove("ScWrapper-uo2e2v-0"));
+
+  // Turn "following" link into "following/live"
+  if (document.querySelector('a[data-a-target="following-link"]').href != "https://www.twitch.tv/directory/following/live") {
+    document.querySelector('a[data-a-target="following-link"]').href = "https://www.twitch.tv/directory/following/live";
+  }
+
 }, 2000);
